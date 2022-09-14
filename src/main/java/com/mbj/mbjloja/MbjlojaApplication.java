5c3d04e3a45c51b09f6bd610c2d076acb7862ae6
Murilo Benatti <benatti1104@gmail.com>
2022-09-13 23:56:28 -0300

    Projeto criado

---
+++ src/main/java/com/mbj/mbjloja/MbjlojaApplication.java
@@ -0,0 +1,13 @@
+package com.mbj.mbjloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbjlojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbjlojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbj/mbjloja/MbjlojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbj.mbjloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbjlojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
